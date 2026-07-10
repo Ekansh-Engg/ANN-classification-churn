75d94599c33bdde6b6a023b5bdff9225e9039558
Ekansh <ekanshagarwal78@gmail.com>
2026-07-10 13:24:50 +0530

    Add files via upload

--- salaryregression_app.py
+++ salaryregression_app.py
@@ -16,7 +16,7 @@ with open ('onehot_encoder_geo.pkl','rb') as file:
 with open('label_encoder_gender.pkl','rb') as file:
     label_encoder_gender=pickle.load(file)
 
-with open('scaler.pkl','rb') as file:
+with open('scaler1.pkl','rb') as file:
     scaler=pickle.load(file)
 
 ## streamlit app
